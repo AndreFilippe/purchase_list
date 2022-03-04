dac19da2013d86718c282941b8b04eef750fc452
André Filippe dos Santos <fellipe.mrt@gmail.com>
2022-03-03 23:20:10 -0300

    test: added mock repository

--- tests/Domain/Services/ShoppingListServiceTest.php
+++ tests/Domain/Services/ShoppingListServiceTest.php
@@ -3,6 +3,7 @@
 namespace Test\Domain\Services;
 
 use App\Domain\Entities\ShoppingList;
+use App\Domain\Repositories\ShoppingListRepositories;
 use App\Domain\Services\ShoppingListService;
 use Test\TestCase;
 
@@ -11,14 +12,25 @@ use Test\TestCase;
  */
 class ShoppingListServiceTest extends TestCase
 {
+    private ShoppingListRepositories $shoppingListRepositories;
+
+    public function setUp(): void
+    {
+        $this->shoppingListRepositories = $this->createMock(ShoppingListRepositories::class);
+
+        $this->shoppingListRepositories->method('save')
+            ->willReturn(new ShoppingList('foo'));
+    }
     /**
      * @test
      */
     public function itShouldReturnShoppingList()
     {
-        $shoppingListService = new ShoppingListService();
+        $shoppingListService = new ShoppingListService($this->shoppingListRepositories);
         $shoppingList = $shoppingListService->create('foo');
 
         $this->assertInstanceOf(ShoppingList::class, $shoppingList);
+
+        $this->assertEquals('foo', $shoppingList->name);
     }
 }
